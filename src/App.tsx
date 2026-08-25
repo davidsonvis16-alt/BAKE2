fbd0e753ba56f3b5f7d12e4fffb4f586275e8ae0
davidsonvis16-alt <davidson.vis.16@gmail.com>
2026-08-25 18:04:33 +0300

    full menu

--- src/App.tsx
+++ src/App.tsx
@@ -290,7 +290,7 @@ export default function App() {
                     <div className="relative mx-auto max-w-[85%] sm:max-w-sm lg:max-w-none">
                       <div className="relative w-full mx-auto">
                         <img
-                          src="/main-bar.png"
+                          src="/full menu image.jpeg"
                           alt="BakeMart signature dish"
                           className="w-full h-auto"
                           onError={(e) => {
